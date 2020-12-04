6b86ad2b2df0095ceb5b01ee11c0c77ce9f463c7
xiaofengshen <835556013@qq.com>
2020-12-04 16:16:57 +0800

    Initial commit

---
+++ app/src/main/java/com/qingmei2/sample/di/ServicesModule.kt
@@ -0,0 +1,27 @@
+package com.qingmei2.sample.di
+
+import com.qingmei2.sample.http.service.ServiceManager
+import com.qingmei2.sample.http.service.UserService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object ServicesModule {
+
+    @Provides
+    @Singleton
+    fun provideUserService(retrofit: Retrofit): UserService {
+        return retrofit.create(UserService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideServiceManager(userService: UserService): ServiceManager {
+        return ServiceManager(userService)
+    }
+}
